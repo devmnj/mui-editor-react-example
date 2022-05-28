f55b0bfa578e856bc0b4096e9584afe8d0316557
Manoj AP <manojap.nair@gmail.com>
2022-05-28 21:52:01 +0530

    Initial commit

---
+++ src/App.js
@@ -0,0 +1,10 @@
+import "./styles.css";
+import MuiEditor from "./MuiRte";
+export default function App() {
+  return (
+    <div className="App">
+      <h1>Hi I am MuiEditor</h1>
+      <MuiEditor />
+    </div>
+  );
+}
